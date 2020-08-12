fb9a71e24eed0983db1ed6691bf1819e5c3cebcc
Musab Nasredlin <musabagab2010@gmail.com>
2020-08-12 11:17:30 +0300

    used the constraint layout to build a simple layout

--- app/src/main/java/com/musab/ad430/MainActivity.kt
+++ app/src/main/java/com/musab/ad430/MainActivity.kt
@@ -2,11 +2,33 @@ package com.musab.ad430
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipCodeEditText:EditText = findViewById(R.id.zipcode_edittext)
+
+        val submitButton:Button = findViewById(R.id.submit_btn)
+
+        submitButton.setOnClickListener {
+            val zipcode = zipCodeEditText.text.toString()
+
+            if (zipcode.length!=5){
+                Toast.makeText(this,R.string.validzipcode,Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,zipcode,Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
+
+
+
 }
